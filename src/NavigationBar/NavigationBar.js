0c1c575fe5ee85e0af00d2f52f3765714b8af2ef
abelmuro <abelmuro93@gmail.com>
2023-07-19 17:03:08 -0700

    commit

--- src/NavigationBar/NavigationBar.js
+++ src/NavigationBar/NavigationBar.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Switch from './Switch';
 import styles from './styles.module.css';
 import logo from './images/logo.svg';
 
-
-
 function NavBar(){
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate('/')
+    }
+
     return(
         <nav className={styles.container}>
             <div className={styles.content}>
-                <img src={logo} className={styles.logo}/>
+                <img src={logo} className={styles.logo} onClick={handleGoBack}/>
                 <Switch/>
             </div>
         </nav>
